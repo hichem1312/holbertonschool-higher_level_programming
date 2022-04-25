95e84c9d1fba32728ef19e9a496303e5a321ecf2
hichem <hichemmaatoug55@gmail.com>
2022-04-25 13:14:51 -0700

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * check_cycle - check the list have a cycle or not
+ * @list: list
+ * Return: 0
+ */
+int check_cycle(listint_t *list)
+{
+	struct listint_s *cycle;
+	cycle = list;
+	while (list->next != NULL)
+	{
+		list = list->next;
+	       if (cycle->next == list->next)
+	       return (1);
+	}
+return (0);
+}
