3d704066e06288fef198afdedc7a0eac935892b0
hichem <hichemmaatoug55@gmail.com>
2022-05-04 16:08:24 -0700

    prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        if i == len(my_list) - 1:
+            print("{:d}".format(my_list[i]))
+        else:
+            print("{:d}".format(my_list[i]))
+
