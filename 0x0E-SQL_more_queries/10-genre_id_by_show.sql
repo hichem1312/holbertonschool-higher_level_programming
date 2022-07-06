be1592ccdbd021bee8cac2e2f9ee0188b239b836
hichem <hichemmaatoug55@gmail.com>
2022-07-06 10:31:41 -0700

    script that lists all shows contained in hbtn_0d_tvshows that have at least one genre

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- display title and genre
+SELECT tv_shows.title AS title, genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id <> 'NULL'
+ORDER BY title, genre_id;
