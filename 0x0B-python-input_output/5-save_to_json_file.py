ded1a186014f4a86a9919a51f85a66e2c7431d8d
hichem <hichemmaatoug55@gmail.com>
2022-07-22 20:47:32 -0700

    d

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     '''define'''
-    with  open(filename, mode="w+") as f:
-         json.dump(my_obj, f)
+    with open(filename, mode="w+") as f:
+        json.dump(my_obj, f)
