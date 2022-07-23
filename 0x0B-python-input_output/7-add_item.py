2fd538e5d93112c5e682e20a2391eec3ccd31830
hichem <hichemmaatoug55@gmail.com>
2022-07-22 20:55:20 -0700

    d

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""importation"""
+from sys import argv
+from os import stat, path
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def main():
+    """define"""
+    file = "add_item.json"
+    if path.isfile(file) and stat(file).st_size != 0:
+        av = load_from_json_file(file)
+    else:
+        av = []
+    av.extend(argv[1:])
+    save_to_json_file(av, file)
+
+
+if __name__ == "__main__":
+    main()
