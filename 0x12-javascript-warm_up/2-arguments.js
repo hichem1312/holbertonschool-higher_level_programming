e1778e1b3c20dd5573d7276b303b3f4c1615f1a0
hichem <hichemmaatoug55@gmail.com>
2022-08-01 09:44:36 -0700

    d

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (argv.length > 3) {
+  console.log('Arguments found');
+} else if (argv.length > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
