7992ed086b91da25dbe141246fed1a267d6ff3b4
hichem <hichemmaatoug55@gmail.com>
2022-08-06 10:18:37 -0700

    no orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list all states"""
+import MySQLdb as sql
+from sys import argv
+
+
+def main(user, password, db):
+    """print states"""
+    db = sql.connect(host='localhost', user=user, password=password, db=db, port=3306)
+    c = db.cursor()
+    c.execute("""SELECT id, name FROM states ORDER BY id;""")
+    x = c.fetchall()
+    for i in x:
+        print(i)
+
+if __name__ == '__main__':
+    main(argv[1], argv[2], argv[3])
