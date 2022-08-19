dbec69cd7cab6f17004f2928dc821f8d8ea81fcd
hichem <hichemmaatoug55@gmail.com>
2022-08-19 09:33:23 -0700

    d

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" sends a request to the URL and displays the value of the variable"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
