47fbd9ac2f4d85cce59b18350f7a2ea6af74f8ee
hichem <hichemmaatoug55@gmail.com>
2022-09-13 09:20:12 -0700

    d

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const v = require('process');
-x = v.argv[2]
-y = v.argv[3]
+const process = require('process');
+x = process.argv[2]
+y = process.argv[3]
 fs.writeFile(x, y, (err) => {
   if (err) throw err;
 });
