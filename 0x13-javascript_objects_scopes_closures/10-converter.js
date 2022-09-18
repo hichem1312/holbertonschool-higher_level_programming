f5fe1f4bba9d6b59bc2d4d4715f3eb380f00c980
hichem <hichem@hichem.com>
2022-09-19 00:58:56 +0100

    d

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    function conv (a) {
-      return (parseInt(a).toString(base));
-    }
-    return conv;
-  };
+  function conv (a) {
+    return (parseInt(a).toString(base));
+  }
+  return conv;
+};
