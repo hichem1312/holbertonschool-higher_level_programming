bd3cc58ddb73241723ddb387344c894805c8fe2f
hichem <hichem@hichem.com>
2022-10-17 05:43:28 +0100

    d

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a model """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
+
+
+class State(Base):
+    """a class"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
