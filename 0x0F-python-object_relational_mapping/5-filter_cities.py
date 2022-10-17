01000d44d8ecc9809d3c1f60456c5d2ab6f0038d
hichem <hichem@hichem.com>
2022-10-17 05:10:00 +0100

    d

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,18 +6,18 @@ from sys import argv
 
 def main(user, password, data, state):
     """print states"""
-    data = sql.connect(host='localhost', user=user
-            , password=password, db=data, port=3306)
+    data = sql.connect(host='localhost', user=user,
+                       password=password, db=data, port=3306)
     a = data.cursor()
     a.execute("""SELECT c.name FROM cities c
             INNER JOIN states s ON s.id = c.state_id
             WHERE s.name = %s
             ORDER BY c.id;""", (state,))
     x = a.fetchall()
-    l = []
+    le = []
     for i in x:
-        l.append(i[0])
-    print(', '.join(l))
+        le.append(i[0])
+    print(', '.join(le))
 
 
 if __name__ == '__main__':
