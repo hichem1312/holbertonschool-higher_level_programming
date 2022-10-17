eb69164212350e69c71318b4550b1609bc5f2637
hichem <hichem@hichem.com>
2022-10-17 05:11:59 +0100

    d

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 def main(user, password, data):
     """print states"""
     engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user, password, data), pool_pre_ping=True)
+                          .format(user, password, data), pool_pre_ping=True)
     Base.metadata.create_all(engin)
     Session = sessionmaker(bind=engin)
     s = Session()
