73148ff6ef987fd390474e224d61b317bd1a4e48
hichem <hichem@hichem.com>
2022-10-17 05:03:52 +0100

    d

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,8 @@ from sys import argv
 
 def main(user, password, data, state):
     """print states"""
-    data = sql.connect(host='localhost', user=user
-            , password=password, db=data, port=3306)
+    data = sql.connect(host='localhost', user=user,
+                       password=password, db=data, port=3306)
     a = data.cursor()
     a.execute("SELECT id, name FROM states \
             WHERE name = '{:s}' ORDER BY id;".format(state))
